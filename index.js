a62ad7c66b3cac6305c40f9f4fa8fbfeab7e17bb
Martin <nitromagix@gmail.com>
2022-08-10 21:08:01 -0700

    cors

--- index.js
+++ index.js
@@ -2,21 +2,19 @@
 
 require("dotenv").config();
 const express = require("express");
-const expressReactViews = require("express-react-views").createEngine();
 const methodOverride = require("method-override");
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.set("views", __dirname + "/views");
-app.set("view engine", "jsx");
-app.engine("jsx", expressReactViews);
+
 
 // Controllers and Routes
-app.use("/places", require("./controllers/places"));
 app.use("/profiles", require("./controllers/profile_controller.js"));
 
 app.get("/", async (req, res) => {
